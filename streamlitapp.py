1347799eae0d035b49f3249a19d51b22e072d238
Anushritha-N <anushrithanbtech24@rvu.edu.in>
2024-11-05 19:33:15 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -33,3 +33,39 @@ fig2 = px.bar(
 )
 fig2.show()
 st.plotly_chart(fig2)
+# ---Task 3: Scatter plot ---
+st.subheader("Task 3:Scatter plot")
+# Scatter Plot: Total Bill vs. Tip (Color-coded by Gender)
+fig3 = px.scatter (
+tips, x='total_bill', y='tip', color='sex',
+title='Total Bill vs Tip(colored by gender)',
+labels={'total_bill': 'Total Bill ($)' , 'tip': 'Tip ($)'},
+template='plotly_dark', #using a cool dark theme 
+size='size', #size of the points based on the size of the group
+)
+fig3.show()
+st.plotly_chart(fig3)
+
+# --- Task 4: Box Plot ---
+st.subheader("Task 4: Box Plot")
+# Box Plot: Distribution of Total Bill by Day (With Color by Time)
+fig4 = px.box(
+    tips, x='day',y='total_bill',color='time',
+    title='Total Bill Distribution by Day and Time',
+    labels={'total_bill': 'Total Bill ($)', 'day':'Day'},
+    template='ggplot2', #classic theme for a beautiful look
+)
+fig4.show()
+st.plotly_chart(fig4)
+
+# --- Task 5: Histogram ---
+st.subheader("Task 5: Histogram")
+# Histogram: Tip Distribution (With Color)
+fig5=px.histogram(
+    tips, x='tip', color='sex',
+    title='Distribution of Tips(Colored by Gender)',
+    labels={'tip': 'Tips ($)', 'sex':'Gender'},
+    template='plotly_white', #clean and bright look
+)
+fig5.show()
+st.plotly_chart(fig5)
